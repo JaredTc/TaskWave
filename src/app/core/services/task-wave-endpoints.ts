cf211ea768bd53239c59a75a68d892d5c8776d67
Jared <jareda_g@outlook.es>
2026-02-02 14:54:17 -0600

    chore: first configs 02/02

---
+++ src/app/core/services/task-wave-endpoints.ts
@@ -0,0 +1,7 @@
+
+export const taskWaveEndpoints = {
+  auth: '/test-taskwave/auth/auth-user/',
+  refresh_token: '',
+  logOut: '',
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  production: false,
+  env_url: 'http://localhost:8000/',
+}
